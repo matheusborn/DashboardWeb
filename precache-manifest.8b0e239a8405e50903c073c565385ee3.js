3e61fafa86a62a4787efda5feda146c6ddf73b4a
Matheus Barros <matheusborn@hotmail.com>
2021-01-19 19:57:18 -0300

    Updates

---
+++ precache-manifest.8b0e239a8405e50903c073c565385ee3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deab68a061a274278192d2684a47b5b5",
+    "url": "/dashboardweb-deploy/index.html"
+  },
+  {
+    "revision": "27a651f51be11ba05b56",
+    "url": "/dashboardweb-deploy/static/css/2.36e35b84.chunk.css"
+  },
+  {
+    "revision": "1a704f2d08358ad73222",
+    "url": "/dashboardweb-deploy/static/css/main.5a3c3026.chunk.css"
+  },
+  {
+    "revision": "27a651f51be11ba05b56",
+    "url": "/dashboardweb-deploy/static/js/2.6d2e7b3d.chunk.js"
+  },
+  {
+    "revision": "6ee1dfbe9947f15dfa54a67e5e3e937e",
+    "url": "/dashboardweb-deploy/static/js/2.6d2e7b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a704f2d08358ad73222",
+    "url": "/dashboardweb-deploy/static/js/main.d9bd89d0.chunk.js"
+  },
+  {
+    "revision": "e1a175780cecd0853fa4",
+    "url": "/dashboardweb-deploy/static/js/runtime-main.90682dfc.js"
+  },
+  {
+    "revision": "33e5dadbb1b685ad13608f95bd7194de",
+    "url": "/dashboardweb-deploy/static/media/Imaki Bold Italic.33e5dadb.otf"
+  },
+  {
+    "revision": "03c9719761430261046ffb711b63d530",
+    "url": "/dashboardweb-deploy/static/media/Imaki Bold.03c97197.otf"
+  },
+  {
+    "revision": "5c6aa825612a654901c1bdacad60a9a2",
+    "url": "/dashboardweb-deploy/static/media/Imaki Condensed Bold Italic.5c6aa825.otf"
+  },
+  {
+    "revision": "962ced8176896a4ed04811c197316539",
+    "url": "/dashboardweb-deploy/static/media/Imaki Condensed Bold.962ced81.otf"
+  },
+  {
+    "revision": "e217a5485d13b5f5bd82dd0acd03d12b",
+    "url": "/dashboardweb-deploy/static/media/Imaki Condensed Italic.e217a548.otf"
+  },
+  {
+    "revision": "0005eddb547a474be17d9c5dea4f7908",
+    "url": "/dashboardweb-deploy/static/media/Imaki Condensed.0005eddb.otf"
+  },
+  {
+    "revision": "a6bd11185d05326e1f377a80dfb79d5a",
+    "url": "/dashboardweb-deploy/static/media/Imaki Italic.a6bd1118.otf"
+  },
+  {
+    "revision": "e1d73782f7886ceeefc41681f175cb88",
+    "url": "/dashboardweb-deploy/static/media/Imaki.e1d73782.otf"
+  },
+  {
+    "revision": "d084e51196d50dd6735ff8a6e4d6f4f2",
+    "url": "/dashboardweb-deploy/static/media/gill-sans-mt.d084e511.ttf"
+  },
+  {
+    "revision": "9a7e4d9221cbff3bda575f0cd7f0e71a",
+    "url": "/dashboardweb-deploy/static/media/logo.9a7e4d92.png"
+  }
+]);
